0a5b69b4643793675249059a015ee472cac51fbf
foisaluddin400 <foisalrk2@gmail.com>
2025-05-19 11:56:31 +0600

    wer

--- src/Pages/ProfilePage/OngoingTicketPage.jsx
+++ src/Pages/ProfilePage/OngoingTicketPage.jsx
@@ -2,6 +2,6 @@ import React from 'react'
 
 export const OngoingTicketPage = () => {
   return (
-    <div>Ongodddddsdffdfgdsfsffdiddfsdfnsdfdgss</div>
+    <div>Ongodddddsdfsdffdfgdsfsffdiddfsdfnsdfdgss</div>
   )
 }
